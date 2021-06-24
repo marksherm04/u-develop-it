c8fac9b31fbebdd31a9844c33e018694600457cb
Mark S <marksherm04@gmail.com>
2021-06-23 22:30:56 -0400

    added query to test database in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const mysql = require("mysql2");
 const express = require("express");
+const mysql = require("mysql2");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -10,16 +11,19 @@ app.use(express.json());
 // Connect to database
 const db = mysql.createConnection(
 	{
-		host: "localhost",
+		host: 'localhost',
 		// Your MySQL username
-		user: "root",
+		user: 'root',
 		//Your MySQL password
-		password: "",
-		database: "election"
+		password: 'Newpass6$',
+		database: 'election'
 	},
 	console.log("Connected to the election database.")
 );
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+	console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
